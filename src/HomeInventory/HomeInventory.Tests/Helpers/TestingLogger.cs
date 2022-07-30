25a4b4a80c7510c42214748cf6cb3ec93dffa945
Serhii A. Hrytsenko <gritcsenko@users.noreply.github.com>
2022-07-30 17:35:37 -0400

    Add LoggingBehaviorTests

---
+++ src/HomeInventory/HomeInventory.Tests/Helpers/TestingLogger.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.Extensions.Logging;
+
+namespace HomeInventory.Tests.Helpers;
+
+public abstract class TestingLogger<T> : ILogger<T>
+{
+    private readonly AsyncLocal<ITestingScope?> _currentScope = new();
+
+    public IDisposable BeginScope<TState>(TState state)
+    {
+        var parent = _currentScope.Value;
+        var scope = new TestingScope<TState>(state, this, parent);
+        _currentScope.Value = scope;
+        return scope;
+    }
+
+    public bool IsEnabled(LogLevel logLevel) => logLevel != LogLevel.None;
+
+    public void Log<TState>(LogLevel logLevel, EventId eventId, TState state, Exception? exception, Func<TState, Exception?, string> formatter)
+        => Log(logLevel, eventId, state!, exception, (object s, Exception? e) => formatter((TState)s, e));
+
+    public abstract void Log(LogLevel logLevel, EventId eventId, object state, Exception? exception, Func<object, Exception?, string> formatter);
+
+    private void EndScope(ITestingScope current) => _currentScope.Value = current.Parent;
+
+    private IEnumerable<ITestingScope> GetScopes()
+    {
+        var scope = _currentScope.Value;
+        while (scope != null)
+        {
+            yield return scope;
+            scope = scope.Parent;
+        }
+    }
+
+    internal interface ITestingScope : IDisposable
+    {
+        ITestingScope? Parent { get; }
+    }
+
+    internal class TestingScope<TState> : ITestingScope
+    {
+        private readonly TState _state;
+        private readonly TestingLogger<T> _logger;
+        private bool _isDisposed;
+
+        public TestingScope(TState state, TestingLogger<T> logger, ITestingScope? parent)
+        {
+            _state = state;
+            _logger = logger;
+            Parent = parent;
+        }
+
+        public ITestingScope? Parent { get; }
+
+        public void Dispose()
+        {
+            if (_isDisposed)
+            {
+                return;
+            }
+
+            _logger.EndScope(this);
+
+            GC.SuppressFinalize(this);
+            _isDisposed = true;
+        }
+    }
+}
---
+++ src/HomeInventory/HomeInventory.Tests/Systems/Handlers/LoggingBehaviorTests.cs
@@ -0,0 +1,69 @@
+﻿using AutoFixture;
+using ErrorOr;
+using FluentAssertions;
+using HomeInventory.Application.Authentication.Behaviors;
+using HomeInventory.Application.Authentication.Queries.Authenticate;
+using HomeInventory.Tests.Customizations;
+using HomeInventory.Tests.Helpers;
+using Microsoft.Extensions.Logging;
+using NSubstitute;
+
+namespace HomeInventory.Tests.Systems.Handlers;
+
+[Trait("Category", "Unit")]
+public class LoggingBehaviorTests : BaseTest
+{
+    private readonly TestingLogger<LoggingBehavior<AuthenticateQuery, ErrorOr<AuthenticateResult>>> _logger;
+    private readonly AuthenticateQuery _request;
+    private readonly ErrorOr<AuthenticateResult> _response;
+
+    public LoggingBehaviorTests()
+    {
+        Fixture.Customize(new UserIdCustomization());
+        _logger = Substitute.For<TestingLogger<LoggingBehavior<AuthenticateQuery, ErrorOr<AuthenticateResult>>>>();
+        _request = Fixture.Create<AuthenticateQuery>();
+        _response = Fixture.Create<AuthenticateResult>();
+    }
+
+    [Fact]
+    public async Task Handle_Should_ReturnResponseFromNext()
+    {
+        var sut = CreateSut();
+
+        var response = await sut.Handle(_request, CancellationToken, () => Task.FromResult(_response));
+
+        response.Value.Should().Be(_response.Value);
+    }
+
+    [Fact]
+    public async Task Handle_Should_LogBeforeCallingNext()
+    {
+        var sut = CreateSut();
+
+        _ = await sut.Handle(_request, CancellationToken, () =>
+        {
+            _logger
+                .Received(1)
+                .Log(LogLevel.Information, new EventId(0), Arg.Any<object>(), null, Arg.Any<Func<object, Exception?, string>>());
+            return Task.FromResult(_response);
+        });
+    }
+
+    [Fact]
+    public async Task Handle_Should_LogAfterCallingNext()
+    {
+        var sut = CreateSut();
+
+        _ = await sut.Handle(_request, CancellationToken, () =>
+        {
+            _logger.ClearReceivedCalls();
+            return Task.FromResult(_response);
+        });
+
+        _logger
+            .Received(1)
+            .Log(LogLevel.Information, new EventId(0), Arg.Any<object>(), null, Arg.Any<Func<object, Exception?, string>>());
+    }
+
+    private LoggingBehavior<AuthenticateQuery, ErrorOr<AuthenticateResult>> CreateSut() => new(_logger);
+}
