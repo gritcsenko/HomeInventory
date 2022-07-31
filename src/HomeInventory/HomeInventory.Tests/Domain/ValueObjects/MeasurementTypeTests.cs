c9f1c1c244ad88f454eff7175e222dcebb476d6a
Serhii A. Hrytsenko <gritcsenko@users.noreply.github.com>
2022-07-31 13:23:41 -0400

    Fix build

--- src/HomeInventory/HomeInventory.Tests/Domain/ValueObjects/MeasurementTypeTests.cs
+++ src/HomeInventory/HomeInventory.Tests/Domain/ValueObjects/MeasurementTypeTests.cs
@@ -9,22 +9,21 @@ public class MeasurementTypeTests : BaseTest
 {
     [Theory]
     [MemberData(nameof(Data))]
-    public void PropertiesShouldMatch(MeasurementType sut, string name, int value)
+    public void PropertiesShouldMatch(MeasurementType sut, string name)
     {
         sut.Name.Should().Be(name);
-        sut.Value.Should().Be(value);
     }
 
-    public static TheoryData<MeasurementType, string, int> Data()
+    public static TheoryData<MeasurementType, string> Data()
     {
         return new()
         {
-            { MeasurementType.Count, nameof(MeasurementType.Count), 0 },
-            { MeasurementType.Length, nameof(MeasurementType.Length), 1 },
-            { MeasurementType.Area, nameof(MeasurementType.Area), 2 },
-            { MeasurementType.Volume, nameof(MeasurementType.Volume), 3 },
-            { MeasurementType.Weight, nameof(MeasurementType.Weight), 4 },
-            { MeasurementType.Temperature, nameof(MeasurementType.Temperature), 5 },
+            { MeasurementType.Count, nameof(MeasurementType.Count) },
+            { MeasurementType.Length, nameof(MeasurementType.Length) },
+            { MeasurementType.Area, nameof(MeasurementType.Area) },
+            { MeasurementType.Volume, nameof(MeasurementType.Volume) },
+            { MeasurementType.Weight, nameof(MeasurementType.Weight) },
+            { MeasurementType.Temperature, nameof(MeasurementType.Temperature) },
         };
     }
 }
