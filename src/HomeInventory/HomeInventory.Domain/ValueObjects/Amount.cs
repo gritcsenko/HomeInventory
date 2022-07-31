c3dd98f8e6c578775d117495b555c244b0cfcf78
Serhii A. Hrytsenko <gritcsenko@users.noreply.github.com>
2022-07-30 20:13:12 -0400

    Add MeasurementTypeTests

--- src/HomeInventory/HomeInventory.Domain/ValueObjects/Amount.cs
+++ src/HomeInventory/HomeInventory.Domain/ValueObjects/Amount.cs
@@ -61,18 +61,3 @@ public class AmountUnit : SmartEnum<AmountUnit>
 
     public static AmountUnit Pieces { get; } = new AmountUnit(nameof(Pieces), MeasurementType.Count, 0);
 }
-
-public class MeasurementType : SmartEnum<MeasurementType>
-{
-    private MeasurementType(string name, int value)
-        : base(name, value)
-    {
-    }
-
-    public static MeasurementType Count { get; } = new MeasurementType(nameof(Count), 0);
-    public static MeasurementType Length { get; } = new MeasurementType(nameof(Length), 1);
-    public static MeasurementType Area { get; } = new MeasurementType(nameof(Area), 2);
-    public static MeasurementType Volume { get; } = new MeasurementType(nameof(Volume), 3);
-    public static MeasurementType Weight { get; } = new MeasurementType(nameof(Weight), 4);
-    public static MeasurementType Temperature { get; } = new MeasurementType(nameof(Temperature), 5);
-}
---
+++ src/HomeInventory/HomeInventory.Tests/Domain/ValueObjects/MeasurementTypeTests.cs
@@ -0,0 +1,30 @@
+﻿using FluentAssertions;
+using HomeInventory.Domain.ValueObjects;
+using HomeInventory.Tests.Helpers;
+
+namespace HomeInventory.Tests.Domain.ValueObjects;
+
+[Trait("Category", "Unit")]
+public class MeasurementTypeTests : BaseTest
+{
+    [Theory]
+    [MemberData(nameof(Data))]
+    public void PropertiesShouldMatch(MeasurementType sut, string name, int value)
+    {
+        sut.Name.Should().Be(name);
+        sut.Value.Should().Be(value);
+    }
+
+    public static TheoryData<MeasurementType, string, int> Data()
+    {
+        return new()
+        {
+            { MeasurementType.Count, nameof(MeasurementType.Count), 0 },
+            { MeasurementType.Length, nameof(MeasurementType.Length), 1 },
+            { MeasurementType.Area, nameof(MeasurementType.Area), 2 },
+            { MeasurementType.Volume, nameof(MeasurementType.Volume), 3 },
+            { MeasurementType.Weight, nameof(MeasurementType.Weight), 4 },
+            { MeasurementType.Temperature, nameof(MeasurementType.Temperature), 5 },
+        };
+    }
+}
