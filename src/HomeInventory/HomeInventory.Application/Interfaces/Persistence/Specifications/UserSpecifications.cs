a34a4f51fe8886c730c8ef310e146a5963f61bdc
Serhii A. Hrytsenko <gritcsenko@users.noreply.github.com>
2022-07-31 14:18:27 -0400

    Add UserRepositoryTests

--- src/HomeInventory/HomeInventory.Application/Interfaces/Persistence/Specifications/UserSpecifications.cs
+++ src/HomeInventory/HomeInventory.Application/Interfaces/Persistence/Specifications/UserSpecifications.cs
@@ -1,8 +1,10 @@
 ﻿using HomeInventory.Domain.Entities;
+using HomeInventory.Domain.ValueObjects;
 
 namespace HomeInventory.Application.Interfaces.Persistence.Specifications;
 
 public static class UserSpecifications
 {
     public static FilterSpecification<User> HasEmail(string email) => new UserHasEmailSpecification(email);
+    public static FilterSpecification<User> HasId(UserId id) => new HasIdSpecification<User, UserId>(id);
 }
