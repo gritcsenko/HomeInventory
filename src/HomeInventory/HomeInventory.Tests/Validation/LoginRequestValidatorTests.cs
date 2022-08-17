9a0933485c7099f04254ee4f7ca152fa1020d641
Serhii A. Hrytsenko <gritcsenko@users.noreply.github.com>
2022-08-17 11:09:59 -0400

    Add LoginRequestValidatorTests

---
+++ src/HomeInventory/HomeInventory.Tests/Validation/LoginRequestValidatorTests.cs
@@ -0,0 +1,54 @@
+﻿using FluentValidation.TestHelper;
+using HomeInventory.Contracts;
+using HomeInventory.Contracts.Validations;
+using HomeInventory.Tests.Helpers;
+
+namespace HomeInventory.Tests.Validation;
+
+public class LoginRequestValidatorTests : BaseTest
+{
+    [Theory]
+    [MemberData(nameof(ValidCases))]
+    public void Should_PassValidation(string? email, string? password)
+    {
+        var container = new LoginRequest(email!, password!);
+        var sut = CreateSut();
+
+        var results = sut.TestValidate(container);
+
+        results.ShouldNotHaveAnyValidationErrors();
+    }
+
+    [Theory]
+    [MemberData(nameof(InvalidCases))]
+    public void Should_NotPassValidation(string? email, string? password)
+    {
+        var container = new LoginRequest(email!, password!);
+        var sut = CreateSut();
+
+        var results = sut.TestValidate(container);
+
+        results.ShouldHaveAnyValidationError();
+    }
+
+    private static LoginRequestValidator CreateSut() => new();
+
+    public static TheoryData<string?, string?> ValidCases() => new()
+    {
+        { "anonymous.user@none.email", "123456789sS$" },
+    };
+
+    public static TheoryData<string?, string?> InvalidCases() => new()
+    {
+        { "none.email", "123456789sS$" },
+        { "", "123456789sS$" },
+        { null, "123456789sS$" },
+        { "anonymous.user@none.email", null },
+        { "anonymous.user@none.email", "" },
+        { "anonymous.user@none.email", "1234sS$" },
+        { "anonymous.user@none.email", "123456789sS" },
+        { "anonymous.user@none.email", "123456789s$" },
+        { "anonymous.user@none.email", "123456789S$" },
+        { "anonymous.user@none.email", "sssssssssS$" },
+    };
+}
