b458bcb18a4fd6e39a9963cbd30c2f8724b07104
Serhii A. Hrytsenko <gritcsenko@users.noreply.github.com>
2022-08-25 10:19:57 -0400

    Add user mapping to and from db model

---
+++ src/HomeInventory/HomeInventory.Infrastructure/Persistence/Mapping/ModelMappings.cs
@@ -0,0 +1,25 @@
+﻿using ErrorOr;
+using HomeInventory.Domain.Entities;
+using HomeInventory.Domain.ValueObjects;
+using HomeInventory.Infrastructure.Persistence.Models;
+using Mapster;
+
+namespace HomeInventory.Infrastructure.Persistence.Mapping;
+internal class ModelMappings : IRegister
+{
+    public void Register(TypeAdapterConfig config)
+    {
+        config.NewConfig<UserId, Guid>().MapWith(id => (Guid)id);
+        config.NewConfig<Guid, UserId>()
+            .ConstructUsing(id => MapContext.Current.GetService<IUserIdFactory>().Create(id)
+                .Match(x => x, ReportError<UserId>)
+            );
+        config.NewConfig<User, UserModel>();
+        config.NewConfig<UserModel, User>();
+    }
+
+    private static T ReportError<T>(IReadOnlyCollection<Error> errors)
+    {
+        throw new InvalidOperationException(errors.First().ToString());
+    }
+}
