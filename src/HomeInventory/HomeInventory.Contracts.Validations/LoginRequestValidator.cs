70405c8b7ab983b0ad0c69eca39a49d9a7d89628
Serhii A. Hrytsenko <gritcsenko@users.noreply.github.com>
2022-09-28 11:01:35 -0400

    Add RegisterRequestValidator

--- src/HomeInventory/HomeInventory.Contracts.Validations/LoginRequestValidator.cs
+++ src/HomeInventory/HomeInventory.Contracts.Validations/LoginRequestValidator.cs
@@ -7,10 +7,7 @@ internal class LoginRequestValidator : AbstractValidator<LoginRequest>
     public LoginRequestValidator()
     {
         RuleFor(x => x.Email).NotEmpty();
-        RuleFor(x => x.Email).EmailAddress();
 
         RuleFor(x => x.Password).NotEmpty();
-        RuleFor(x => x.Password).MinimumLength(8);
-        RuleFor(x => x.Password).Password();
     }
 }
---
+++ src/HomeInventory/HomeInventory.Contracts.Validations/RegisterRequestValidator.cs
@@ -0,0 +1,20 @@
+﻿using FluentValidation;
+
+namespace HomeInventory.Contracts.Validations;
+
+internal class RegisterRequestValidator : AbstractValidator<RegisterRequest>
+{
+    public RegisterRequestValidator()
+    {
+        RuleFor(x => x.FirstName).NotEmpty();
+
+        RuleFor(x => x.LastName).NotEmpty();
+
+        RuleFor(x => x.Email).NotEmpty();
+        RuleFor(x => x.Email).EmailAddress();
+
+        RuleFor(x => x.Password).NotEmpty();
+        RuleFor(x => x.Password).MinimumLength(8);
+        RuleFor(x => x.Password).Password();
+    }
+}
