03773873c5b7d642c6e0864248898db6ba676d68
Serhii A. Hrytsenko <gritcsenko@users.noreply.github.com>
2022-10-04 22:28:29 -0400

    Refactor driver

--- src/HomeInventory/HomeInventory.Tests.Acceptance/Drivers/HomeInventoryAPIDriver.cs
+++ src/HomeInventory/HomeInventory.Tests.Acceptance/Drivers/HomeInventoryAPIDriver.cs
@@ -1,10 +1,10 @@
 ﻿using HomeInventory.Domain.Primitives;
 using HomeInventory.Infrastructure.Persistence;
+using HomeInventory.Tests.Helpers;
 using HomeInventory.Tests.Support;
 using Microsoft.AspNetCore.Mvc.Testing;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.DependencyInjection.Extensions;
 using Microsoft.Extensions.Hosting;
 
 namespace HomeInventory.Tests.Acceptance.Drivers;
@@ -30,15 +30,14 @@ internal class HomeInventoryAPIDriver : WebApplicationFactory<Program>, IHomeInv
         // Add mock/test services to the builder here
         builder.ConfigureServices(services =>
         {
-            services.RemoveAll<DbContextOptions<DatabaseContext>>();
             var id = Guid.NewGuid();
             // Replace real database with in-memory database for tests
-            services.AddScoped(sp => new DbContextOptionsBuilder<DatabaseContext>()
+            services.ReplaceWithScoped(sp => new DbContextOptionsBuilder<DatabaseContext>()
                 .UseApplicationServiceProvider(sp)
                 .UseInMemoryDatabase($"HomeInventory{id:D}")
                 .Options);
             services.AddSingleton<FixedTestingDateTimeService>();
-            services.AddSingleton<IDateTimeService>(sp => sp.GetRequiredService<FixedTestingDateTimeService>());
+            services.ReplaceWithSingleton<IDateTimeService>(sp => sp.GetRequiredService<FixedTestingDateTimeService>());
         });
 
         return base.CreateHost(builder);
