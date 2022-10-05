98bbb7dcbe6335bca5c8b7366a54106365700792
Serhii A. Hrytsenko <gritcsenko@users.noreply.github.com>
2022-10-04 22:39:31 -0400

    Fix formatting

--- src/HomeInventory/HomeInventory.Tests.Acceptance/Support/StringExtensions.cs
+++ src/HomeInventory/HomeInventory.Tests.Acceptance/Support/StringExtensions.cs
@@ -3,5 +3,6 @@
 internal static class StringExtensions
 {
     public static DateOnly ParseDate(this string text, string format = "MM/dd/yyyy") => DateOnly.ParseExact(text, format);
+
     public static decimal ParseDecimal(this string text) => decimal.Parse(text);
 }
