d3119ae41f0f6f293cc5259277b62d7a19c913ca
Serhii A. Hrytsenko <gritcsenko@users.noreply.github.com>
2022-10-11 15:58:13 -0400

    Add Serilog

--- src/HomeInventory/HomeInventory.Api/Program.cs
+++ src/HomeInventory/HomeInventory.Api/Program.cs
@@ -3,25 +3,46 @@ using HomeInventory.Domain;
 using HomeInventory.Infrastructure;
 using HomeInventory.Web;
 using MediatR;
+using Serilog;
 
-var builder = WebApplication.CreateBuilder(args);
+Log.Logger = new LoggerConfiguration()
+    .WriteTo.Console()
+    .CreateBootstrapLogger();
+try
+{
+    var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddMediatR(HomeInventory.Application.AssemblyReference.Assembly, HomeInventory.Infrastructure.AssemblyReference.Assembly);
+    builder.Host.UseSerilog(ConfigureSerilog, preserveStaticLogger: false, writeToProviders: false);
+    builder.Services.AddMediatR(HomeInventory.Application.AssemblyReference.Assembly, HomeInventory.Infrastructure.AssemblyReference.Assembly);
 
-builder.Services.AddDomain();
-builder.Services.AddInfrastructure();
-builder.Services.AddApplication();
-builder.Services.AddWeb();
+    builder.Services.AddDomain();
+    builder.Services.AddInfrastructure();
+    builder.Services.AddApplication();
+    builder.Services.AddWeb();
 
-var app = builder.Build();
-app.UseWeb();
+    var app = builder.Build();
+    app.UseWeb();
 
-app.UseHttpsRedirection();
+    app.UseHttpsRedirection();
 
-app.UseAuthentication();
-app.UseAuthorization();
+    app.UseAuthentication();
+    app.UseAuthorization();
 
-app.MapControllers();
-app.Run();
+    app.MapControllers();
+    app.Run();
+}
+catch (Exception ex)
+{
+    Log.Fatal(ex, "An unhandled exception occured during bootstrapping");
+}
+finally
+{
+    Log.CloseAndFlush();
+}
+
+static void ConfigureSerilog(HostBuilderContext context, IServiceProvider services, LoggerConfiguration configuration) =>
+    configuration
+        .ReadFrom.Configuration(context.Configuration)
+        .ReadFrom.Services(services);
 
 public partial class Program { }
