4f47b23a0b72d425e222341a249e3efadf7749f8
Serhii A. Hrytsenko <gritcsenko@users.noreply.github.com>
2023-01-08 12:29:52 -0500

    Add persistent configurations for StorageArea

---
+++ src/HomeInventory/HomeInventory.Infrastructure/Persistence/Models/Configurations/ProductModelConfiguration.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace HomeInventory.Infrastructure.Persistence.Models.Configurations;
+
+internal class ProductModelConfiguration : IEntityTypeConfiguration<ProductModel>
+{
+    public void Configure(EntityTypeBuilder<ProductModel> builder)
+    {
+        builder.ToTable("Products");
+
+        builder.HasKey(x => x.Id);
+
+        builder.Property(x => x.Name)
+            .IsRequired();
+
+        builder.HasOne(x => x.Amount)
+            .WithOne()
+            .HasForeignKey<ProductAmountModel>("id");
+        builder.Navigation(x => x.Amount).IsRequired();
+    }
+}
---
+++ src/HomeInventory/HomeInventory.Infrastructure/Persistence/Models/Configurations/StorageAreaModelConfiguration.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace HomeInventory.Infrastructure.Persistence.Models.Configurations;
+
+internal class StorageAreaModelConfiguration : IEntityTypeConfiguration<StorageAreaModel>
+{
+    public void Configure(EntityTypeBuilder<StorageAreaModel> builder)
+    {
+        builder.HasKey(x => x.Id);
+
+        builder.Property(x => x.Name)
+            .IsRequired();
+    }
+}
