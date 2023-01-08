c173883c402d53f642edca4fe0b152b8412c6e56
Serhii A. Hrytsenko <gritcsenko@users.noreply.github.com>
2023-01-08 12:09:21 -0500

    Add GetOrAdd method to DictionaryExtensions

--- src/HomeInventory/HomeInventory.Domain.Primitives/Extensions/DictionaryExtensions.cs
+++ src/HomeInventory/HomeInventory.Domain.Primitives/Extensions/DictionaryExtensions.cs
@@ -6,4 +6,7 @@ public static class DictionaryExtensions
 {
     public static Result<TValue> GetValueOrFail<TKey, TValue>(this IReadOnlyDictionary<TKey, TValue> dictionary, TKey key, Func<TKey, IError> createErrorFunc)
         => dictionary.TryGetValue(key, out var value) ? Result.Ok(value) : Result.Fail<TValue>(createErrorFunc(key));
+
+    public static TValue GetOrAdd<TKey, TValue>(this IDictionary<TKey, TValue> dictionary, TKey key, Func<TKey, TValue> getValueFunc)
+        => dictionary.TryGetValue(key, out var value) ? value : dictionary[key] = getValueFunc(key);
 }
