6ced26c705ec4930de56f002a48a9a64f0c90612
Serhii A. Hrytsenko <gritcsenko@users.noreply.github.com>
2023-01-08 12:09:50 -0500

    Add ForEach to EnumerableExtensions

---
+++ src/HomeInventory/HomeInventory.Domain.Primitives/Extensions/EnumerableExtensions.cs
@@ -0,0 +1,12 @@
+﻿namespace HomeInventory.Domain;
+
+public static class EnumerableExtensions
+{
+    public static void ForEach<T>(this IEnumerable<T> source, Action<T> action)
+    {
+        foreach (var item in source)
+        {
+            action(item);
+        }
+    }
+}
