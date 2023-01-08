8149658246d69123499bd0b5e17b207507420cb3
Serhii A. Hrytsenko <gritcsenko@users.noreply.github.com>
2023-01-08 12:10:33 -0500

    Move Fill from IPool to IPool<T>

--- src/HomeInventory/HomeInventory.Domain.Primitives/IPool.cs
+++ src/HomeInventory/HomeInventory.Domain.Primitives/IPool.cs
@@ -5,11 +5,10 @@ public interface IPool
     int Count { get; }
 
     void Clear();
-
-    void Fill(int count);
 }
 
 public interface IPool<T> : IPool, IPoolObjectActivator<T>
     where T : class
 {
+    void Fill(int count);
 }
