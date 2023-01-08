a36ea00a7583e445c2a6695d9a16cef74545af14
Serhii A. Hrytsenko <gritcsenko@users.noreply.github.com>
2023-01-08 12:09:36 -0500

    Add DequeueOrDefault to QueueExtensions

---
+++ src/HomeInventory/HomeInventory.Domain.Primitives/Extensions/QueueExtensions.cs
@@ -0,0 +1,6 @@
+﻿namespace HomeInventory.Domain;
+
+public static class QueueExtensions
+{
+    public static T DequeueOrDefault<T>(this Queue<T> queue, Func<T> getDefautFunc) => queue.TryDequeue(out var value) ? value : getDefautFunc();
+}
