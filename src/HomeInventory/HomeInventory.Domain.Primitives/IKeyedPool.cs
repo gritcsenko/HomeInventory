01482de35bf831edf04ef6872a66d03067c0d7fc
Serhii A. Hrytsenko <gritcsenko@users.noreply.github.com>
2023-01-08 12:10:43 -0500

    Add KeyedPool

---
+++ src/HomeInventory/HomeInventory.Domain.Primitives/IKeyedPool.cs
@@ -0,0 +1,8 @@
+﻿namespace HomeInventory.Domain.Primitives;
+
+public interface IKeyedPool<TKey, T> : IPool
+    where TKey : IEquatable<TKey>
+    where T : class
+{
+    IPool<T> Get(TKey key);
+}
---
+++ src/HomeInventory/HomeInventory.Domain.Primitives/KeyedPool.cs
@@ -0,0 +1,17 @@
+﻿namespace HomeInventory.Domain.Primitives;
+
+public class KeyedPool<TKey, T> : IKeyedPool<TKey, T>
+    where TKey : IEquatable<TKey>
+    where T : class
+{
+    private readonly Dictionary<TKey, IPool<T>> _pools = new();
+    private readonly Func<TKey, IPool<T>> _poolFactory;
+
+    public KeyedPool(Func<TKey, IPool<T>> poolFactory) => _poolFactory = poolFactory;
+
+    public int Count => _pools.Values.Sum(pool => pool.Count);
+
+    public void Clear() => _pools.Values.ForEach(pool => pool.Clear());
+
+    public IPool<T> Get(TKey key) => _pools.GetOrAdd(key, _poolFactory);
+}
