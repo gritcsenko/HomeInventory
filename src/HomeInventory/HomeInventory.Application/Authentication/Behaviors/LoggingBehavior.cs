cef78b8929cab49c9778aeac73c38fc90ef27166
Serhii A. Hrytsenko <gritcsenko@users.noreply.github.com>
2023-01-13 13:46:23 -0500

    Update mediator logging

--- src/HomeInventory/HomeInventory.Application/Authentication/Behaviors/LoggingBehavior.cs
+++ src/HomeInventory/HomeInventory.Application/Authentication/Behaviors/LoggingBehavior.cs
@@ -1,11 +1,12 @@
-﻿using HomeInventory.Domain;
+﻿using FluentResults;
+using HomeInventory.Domain;
 using MediatR;
 using Microsoft.Extensions.Logging;
 
 namespace HomeInventory.Application.Authentication.Behaviors;
 
-internal class LoggingBehavior<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse>
-     where TRequest : IRequest<TResponse>
+internal class LoggingBehavior<TRequest, TResponse> : IPipelineBehavior<TRequest, Result<TResponse>>
+    where TRequest : IRequest<Result<TResponse>>
 {
     private static readonly string RequestName = typeof(TRequest).GetFormattedName();
     private static readonly string ResponseName = typeof(TResponse).GetFormattedName();
@@ -14,11 +15,18 @@ internal class LoggingBehavior<TRequest, TResponse> : IPipelineBehavior<TRequest
 
     public LoggingBehavior(ILogger<LoggingBehavior<TRequest, TResponse>> logger) => _logger = logger;
 
-    public async Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
+    public async Task<Result<TResponse>> Handle(TRequest request, RequestHandlerDelegate<Result<TResponse>> next, CancellationToken cancellationToken)
     {
         _logger.LogInformation("{Request} was sent and expecting {Response}", RequestName, ResponseName);
         var response = await next();
-        _logger.LogInformation("{Request} was handled and {Response} was returned", RequestName, ResponseName);
+        if (response.IsFailed)
+        {
+            _logger.LogError("{@Request} was failed with error(s): {@Error}", RequestName, string.Join("; ", response.Errors.Select(e => e.Message)));
+        }
+        else
+        {
+            _logger.LogInformation("{Request} was handled and {Response} was returned", RequestName, ResponseName);
+        }
         return response;
     }
 }
