7e9e36e1280f2c408ccfbfe1efc630d5dec73f07
Serhii A. Hrytsenko <gritcsenko@users.noreply.github.com>
2023-02-24 10:29:19 -0500

    Add disposables

---
+++ src/HomeInventory/HomeInventory.Domain.Primitives/AsyncDisposable.cs
@@ -0,0 +1,26 @@
+﻿namespace HomeInventory.Domain.Primitives;
+
+public class AsyncDisposable : IAsyncDisposable
+{
+    public static IAsyncDisposable None { get; } = new AsyncDisposable();
+
+    public bool IsDisposed { get; private set; }
+
+    public async ValueTask DisposeAsync()
+    {
+        if (IsDisposed)
+        {
+            return;
+        }
+
+        IsDisposed = true;
+        await InternalDisposeAsync();
+        GC.SuppressFinalize(this);
+    }
+
+    protected virtual ValueTask InternalDisposeAsync()
+    {
+        return ValueTask.CompletedTask;
+    }
+
+}
---
+++ src/HomeInventory/HomeInventory.Domain.Primitives/Disposable.cs
@@ -0,0 +1,28 @@
+﻿namespace HomeInventory.Domain.Primitives;
+
+public class Disposable : IDisposable
+{
+    public static IDisposable None { get; } = new Disposable();
+
+    public bool IsDisposed { get; private set; }
+
+    public static IDisposable Create(Action action) => new DisposeAction(action);
+
+    public static IDisposable Create<T>(Action<T> action, T state) => new DisposeAction<T>(action, state);
+
+    public void Dispose()
+    {
+        if (IsDisposed)
+        {
+            return;
+        }
+
+        IsDisposed = true;
+        InternalDispose();
+        GC.SuppressFinalize(this);
+    }
+
+    protected virtual void InternalDispose()
+    {
+    }
+}
