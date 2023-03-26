94135e7861c7c6c8cd326c7878af8a4d2ce172bd
Serhii A. Hrytsenko <gritcsenko@users.noreply.github.com>
2023-03-26 11:27:55 -0400

    Add UserIdQuery

---
+++ src/HomeInventory/HomeInventory.Application/Cqrs/Queries/UserId/UserIdQuery.cs
@@ -0,0 +1,6 @@
+﻿using HomeInventory.Application.Interfaces.Messaging;
+
+namespace HomeInventory.Application.Cqrs.Queries.UserId;
+
+public record UserIdQuery(
+    string Email) : IQuery<UserIdResult>;
---
+++ src/HomeInventory/HomeInventory.Application/Cqrs/Queries/UserId/UserIdQueryHandler.cs
@@ -0,0 +1,28 @@
+﻿using FluentResults;
+using HomeInventory.Application.Interfaces.Messaging;
+using HomeInventory.Application.Interfaces.Persistence;
+using HomeInventory.Application.Interfaces.Persistence.Specifications;
+using HomeInventory.Domain.Errors;
+
+namespace HomeInventory.Application.Cqrs.Queries.UserId;
+
+internal class UserIdQueryHandler : QueryHandler<UserIdQuery, UserIdResult>
+{
+    private readonly IUserRepository _repository;
+
+    public UserIdQueryHandler(IUserRepository userRepository)
+    {
+        _repository = userRepository;
+    }
+
+    protected override async Task<Result<UserIdResult>> InternalHandle(UserIdQuery query, CancellationToken cancellationToken)
+    {
+        var result = await _repository.FindFirstOrNotFoundAsync(UserSpecifications.HasEmail(query.Email), cancellationToken);
+        if (result.TryPickT1(out _, out var user))
+        {
+            return new NotFoundError($"User with email {query.Email} was not found");
+        }
+
+        return new UserIdResult(user.Id);
+    }
+}
