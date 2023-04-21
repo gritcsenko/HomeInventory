55fb4156ef3d69ca1f2d3c8cdda3e5dadf7e6752
Serhii A. Hrytsenko <gritcsenko@users.noreply.github.com>
2023-04-21 09:43:02 -0400

    Fix build

--- src/HomeInventory/HomeInventory.Domain.Primitives/ValueObjectFactory.cs
+++ src/HomeInventory/HomeInventory.Domain.Primitives/ValueObjectFactory.cs
@@ -1,5 +1,4 @@
-﻿using HomeInventory.Domain.Errors;
-using HomeInventory.Domain.Primitives.Errors;
+﻿using HomeInventory.Domain.Primitives.Errors;
 using OneOf;
 
 namespace HomeInventory.Domain.Primitives;
