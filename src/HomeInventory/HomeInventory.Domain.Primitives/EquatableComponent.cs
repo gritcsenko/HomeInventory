bc83211f2e5ea60b2bd15e3dac4aed6d074fe41f
Serhii A. Hrytsenko <gritcsenko@users.noreply.github.com>
2023-04-24 11:19:16 -0400

    Add EquatableComponent tests

--- src/HomeInventory/HomeInventory.Domain.Primitives/EquatableComponent.cs
+++ src/HomeInventory/HomeInventory.Domain.Primitives/EquatableComponent.cs
@@ -4,9 +4,20 @@ public readonly struct EquatableComponent<T>
 {
     private readonly IReadOnlyCollection<object> _components;
 
+    public EquatableComponent()
+        : this(Array.Empty<object>())
+    {
+    }
+
     public EquatableComponent(params object[] components) => _components = components;
 
-    public override int GetHashCode() => _components.Aggregate(default(int), HashCode.Combine);
+    public override int GetHashCode() => _components.Aggregate(new HashCode(), Combine).ToHashCode();
 
     public bool Equals(EquatableComponent<T> other) => _components.SequenceEqual(other._components);
+
+    private static HashCode Combine(HashCode hash, object obj)
+    {
+        hash.Add(obj);
+        return hash;
+    }
 }
---
+++ src/HomeInventory/HomeInventory.Tests/Domain/EquatableComponentTests.cs
@@ -0,0 +1,88 @@
+﻿using HomeInventory.Domain.Primitives;
+
+namespace HomeInventory.Tests.Domain;
+
+[UnitTest]
+public class EquatableComponentTests : BaseTest
+{
+    [Fact]
+    public void GetHashCode_ShoudReturnZero_WhenNoComponents()
+    {
+        var sut = new EquatableComponent<string>();
+
+        var actual = sut.GetHashCode();
+
+        actual.Should().Be(new HashCode().ToHashCode());
+    }
+
+    [Theory]
+    [InlineData(1)]
+    [InlineData(2)]
+    [InlineData(3)]
+    public void GetHashCode_ShoudReturnCombinedComponentsHash_WhenManyComponents(int count)
+    {
+        var components = Fixture.CreateMany<Guid>(count).Cast<object>().ToArray();
+        var expected = new HashCode();
+        foreach (var component in components)
+        {
+            expected.Add(component);
+        }
+        var sut = new EquatableComponent<string>(components);
+
+        var actual = sut.GetHashCode();
+
+        actual.Should().Be(expected.ToHashCode());
+    }
+
+    [Fact]
+    public void Equals_ShoudBeEqualToEmpty_WhenNoComponents()
+    {
+        var sut = new EquatableComponent<string>();
+
+        var actual = sut.Equals(new EquatableComponent<string>());
+
+        actual.Should().BeTrue();
+    }
+
+    [Theory]
+    [InlineData(1)]
+    [InlineData(2)]
+    [InlineData(3)]
+    public void Equals_ShoudNotBeEqualToEmpty_WhenManyComponents(int count)
+    {
+        var components = Fixture.CreateMany<Guid>(count).Cast<object>().ToArray();
+        var sut = new EquatableComponent<string>(components);
+
+        var actual = sut.Equals(new EquatableComponent<string>());
+
+        actual.Should().BeFalse();
+    }
+
+    [Theory]
+    [InlineData(1)]
+    [InlineData(2)]
+    [InlineData(3)]
+    public void Equals_ShoudBeEqualToComponentWithSameItems_WhenManyComponents(int count)
+    {
+        var components = Fixture.CreateMany<Guid>(count).Cast<object>().ToArray();
+        var sut = new EquatableComponent<string>(components);
+
+        var actual = sut.Equals(new EquatableComponent<string>(components));
+
+        actual.Should().BeTrue();
+    }
+
+    [Theory]
+    [InlineData(1)]
+    [InlineData(2)]
+    [InlineData(3)]
+    public void Equals_ShoudNotBeEqualToComponentWithDifferentItems_WhenManyComponents(int count)
+    {
+        var components = Fixture.CreateMany<Guid>(count).Cast<object>().ToArray();
+        var sut = new EquatableComponent<string>(components);
+
+        var actual = sut.Equals(new EquatableComponent<string>(Fixture.CreateMany<Guid>(count).Cast<object>().ToArray()));
+
+        actual.Should().BeFalse();
+    }
+}
