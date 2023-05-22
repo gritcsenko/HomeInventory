e9d19a0cefd9e715c70d9caf6d6d4de17cf6543b
Serhii A. Hrytsenko <gritcsenko@users.noreply.github.com>
2023-05-22 11:11:52 -0400

    Update GetInstanceOf

--- src/HomeInventory/HomeInventory.Tests/BaseTest.cs
+++ src/HomeInventory/HomeInventory.Tests/BaseTest.cs
@@ -20,10 +20,10 @@ public abstract class BaseTest : Disposable
 
     protected IDateTimeService DateTime => _lazyDateTime.Value;
 
-    protected static object GetInstanceOf(Type type)
+    protected static object? GetInstanceOf(Type type)
     {
         if (type.GetConstructor(Type.EmptyTypes) != null)
-            return Activator.CreateInstance(type)!;
+            return Activator.CreateInstance(type);
 
         // Type without parameterless constructor
         return FormatterServices.GetUninitializedObject(type);
