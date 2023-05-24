82f2b1cf8d163e23af98c2720d79598dae3d3868
Serhii A. Hrytsenko <gritcsenko@users.noreply.github.com>
2023-05-24 19:46:45 -0400

    Refactor AmountFactory

--- src/HomeInventory/HomeInventory.Domain/ValueObjects/AmountFactory.cs
+++ src/HomeInventory/HomeInventory.Domain/ValueObjects/AmountFactory.cs
@@ -4,26 +4,42 @@ using OneOf;
 
 namespace HomeInventory.Domain.ValueObjects;
 
-internal sealed class AmountFactory : ValueObjectFactory<Amount>, IAmountFactory
+internal sealed class AmountFactory : IAmountFactory
 {
-    private static readonly IReadOnlyDictionary<AmountUnit, Func<decimal, bool>> Validators = new Dictionary<AmountUnit, Func<decimal, bool>>
-    {
-        [AmountUnit.Kelvin] = x => x >= 0m,
-    };
+    public OneOf<Amount, IError> Create(decimal value, AmountUnit unit) =>
+        TryCreate((value, unit), x => Validator.Validate(x.value, x.unit), x => new Amount(x.value, x.unit));
 
-    public OneOf<Amount, IError> Create(decimal value, AmountUnit unit)
+    private static OneOf<Amount, IError> TryCreate<TValue>(TValue value, Func<TValue, bool> isValidFunc, Func<TValue, Amount> createFunc)
     {
-        var validateFunc = Validators.GetValueOrDefault(unit, SelectValidator);
-        return TryCreate((value, unit), x => validateFunc(x.value), x => new Amount(x.value, x.unit));
+        if (isValidFunc(value))
+        {
+            return createFunc(value);
+        }
+
+        return new ObjectValidationError<TValue>(value);
     }
 
-    private Func<decimal, bool> SelectValidator(AmountUnit unit) => unit switch
+    private class Validator
     {
-        { } u when u.Measurement == MeasurementType.Count => x => x >= 0m && decimal.Truncate(x) == x,
-        { } u when u.Measurement == MeasurementType.Volume => x => x >= 0m,
-        { } u when u.Measurement == MeasurementType.Area => x => x >= 0m,
-        { } u when u.Measurement == MeasurementType.Length => x => x >= 0m,
-        { } u when u.Measurement == MeasurementType.Weight => x => x >= 0m,
-        _ => x => true,
-    };
+        private static readonly IReadOnlyDictionary<AmountUnit, Func<decimal, bool>> _validators = new Dictionary<AmountUnit, Func<decimal, bool>>
+        {
+            [AmountUnit.Kelvin] = x => x >= 0m,
+        };
+
+        public static bool Validate(decimal value, AmountUnit unit)
+        {
+            var validateFunc = _validators.GetValueOrDefault(unit, SelectValidator);
+            return validateFunc(value);
+        }
+
+        private static Func<decimal, bool> SelectValidator(AmountUnit unit) => unit switch
+        {
+            { } u when u.Measurement == MeasurementType.Count => x => x >= 0m && decimal.Truncate(x) == x,
+            { } u when u.Measurement == MeasurementType.Volume => x => x >= 0m,
+            { } u when u.Measurement == MeasurementType.Area => x => x >= 0m,
+            { } u when u.Measurement == MeasurementType.Length => x => x >= 0m,
+            { } u when u.Measurement == MeasurementType.Weight => x => x >= 0m,
+            _ => x => true,
+        };
+    }
 }
