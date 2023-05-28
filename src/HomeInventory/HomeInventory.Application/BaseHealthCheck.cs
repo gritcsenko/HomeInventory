d331132fb51f8dd078fc8f3660d70325c6d07d55
Serhii A. Hrytsenko <gritcsenko@users.noreply.github.com>
2023-05-28 15:30:41 -0400

    Refactor PersistenceHealthCheck

---
+++ src/HomeInventory/HomeInventory.Application/BaseHealthCheck.cs
@@ -0,0 +1,33 @@
+﻿using DotNext;
+using HomeInventory.Domain.Primitives;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+
+namespace HomeInventory.Application;
+
+public abstract class BaseHealthCheck : IHealthCheck
+{
+    public async Task<HealthCheckResult> CheckHealthAsync(HealthCheckContext context, CancellationToken cancellationToken = default)
+    {
+        var (token, resources) = cancellationToken.WithTimeout(context.Registration.Timeout);
+        try
+        {
+            var isHealthy = await IsHealthyAsync(token);
+            if (isHealthy)
+            {
+                return HealthCheckResult.Healthy();
+            }
+
+            return new HealthCheckResult(context.Registration.FailureStatus);
+        }
+        catch (Exception ex)
+        {
+            return new HealthCheckResult(context.Registration.FailureStatus, exception: ex);
+        }
+        finally
+        {
+            Disposable.Dispose(resources);
+        }
+    }
+
+    protected abstract ValueTask<bool> IsHealthyAsync(CancellationToken cancellationToken);
+}
--- src/HomeInventory/HomeInventory.Infrastructure/Services/PersistenceHealthCheck.cs
+++ src/HomeInventory/HomeInventory.Infrastructure/Services/PersistenceHealthCheck.cs
@@ -1,24 +1,18 @@
-﻿using HomeInventory.Infrastructure.Persistence;
+﻿using HomeInventory.Application;
+using HomeInventory.Infrastructure.Persistence;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Diagnostics.HealthChecks;
 
 namespace HomeInventory.Infrastructure.Services;
 
-internal class PersistenceHealthCheck : IHealthCheck
+internal class PersistenceHealthCheck : BaseHealthCheck
 {
     private readonly IDbContextFactory<DatabaseContext> _factory;
 
     public PersistenceHealthCheck(IDbContextFactory<DatabaseContext> contextFactory) => _factory = contextFactory;
 
-    public async Task<HealthCheckResult> CheckHealthAsync(HealthCheckContext context, CancellationToken cancellationToken = default)
+    protected override async ValueTask<bool> IsHealthyAsync(CancellationToken cancellationToken)
     {
         await using var dbContext = await _factory.CreateDbContextAsync(cancellationToken);
-        var canConnect = await dbContext.Database.CanConnectAsync(cancellationToken);
-        if (canConnect)
-        {
-            return HealthCheckResult.Healthy();
-        }
-
-        return HealthCheckResult.Unhealthy();
+        return await dbContext.Database.CanConnectAsync(cancellationToken);
     }
 }
