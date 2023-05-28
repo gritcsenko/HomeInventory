2fb77dca812f090091ddffd8c08a3214bded0205
Serhii A. Hrytsenko <gritcsenko@users.noreply.github.com>
2023-05-28 12:55:44 -0400

    Remove EnumerableExtensions from tests

--- src/HomeInventory/HomeInventory.Tests/EnumerableExtensions.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace HomeInventory.Tests;
-
-internal static class EnumerableExtensions
-{
-    public static IEnumerable<T> Concat<T>(this IEnumerable<T> source, T item) => source.Concat(Enumerable.Repeat(item, 1));
-}
