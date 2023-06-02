4ca3b2a3cd81cae6fe6f29b53c0177687c04d393
Serhii A. Hrytsenko <gritcsenko@users.noreply.github.com>
2023-06-02 11:16:11 -0400

    Add UserIdQueryHandlerTests

---
+++ src/HomeInventory/HomeInventory.Tests/Systems/Handlers/UserIdQueryHandlerTests.cs
@@ -0,0 +1,61 @@
+﻿using HomeInventory.Application.Cqrs.Queries.UserId;
+using HomeInventory.Domain.Aggregates;
+using HomeInventory.Domain.Persistence;
+using HomeInventory.Domain.Primitives.Errors;
+using HomeInventory.Domain.ValueObjects;
+
+namespace HomeInventory.Tests.Systems.Handlers;
+
+[UnitTest]
+public class UserIdQueryHandlerTests : BaseTest
+{
+    private readonly IUserRepository _userRepository = Substitute.For<IUserRepository>();
+    private readonly User _user;
+
+    public UserIdQueryHandlerTests()
+    {
+        Fixture.CustomizeGuidId<UserId>();
+        Fixture.CustomizeEmail();
+        _user = Fixture.Create<User>();
+    }
+
+    private UserIdQueryHandler CreateSut() => new(_userRepository);
+
+    [Fact]
+    public async Task Handle_OnSuccess_ReturnsResult()
+    {
+        // Given
+        var query = new UserIdQuery(_user.Email);
+
+        _userRepository.FindFirstByEmailUserOptionalAsync(query.Email, Cancellation.Token).Returns(_user);
+
+        var sut = CreateSut();
+        // When
+        var result = await sut.Handle(query, Cancellation.Token);
+        // Then
+        result.Should().NotBeNull();
+        result.Index.Should().Be(0);
+        var subject = result.Value
+            .Should().BeOfType<UserIdResult>()
+            .Subject;
+        subject.UserId.Should().Be(_user.Id);
+    }
+
+    [Fact]
+    public async Task Handle_OnNotFound_ReturnsError()
+    {
+        // Given
+        var query = Fixture.Create<UserIdQuery>();
+        _userRepository.FindFirstByEmailUserOptionalAsync(query.Email, Cancellation.Token).Returns(Optional.None<User>());
+
+        var sut = CreateSut();
+        // When
+        var result = await sut.Handle(query, Cancellation.Token);
+        // Then
+        result.Should().NotBeNull();
+        result.Index.Should().Be(1);
+        result.Value.Should().BeAssignableTo<IError>()
+           .Which.Should().BeOfType<NotFoundError>()
+           .Which.Message.Should().Contain(query.Email.ToString());
+    }
+}
