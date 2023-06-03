bc42bc894ef537288f48f8a4c5a8067f647adcc1
Serhii A. Hrytsenko <gritcsenko@users.noreply.github.com>
2023-06-03 14:49:04 -0400

    Add UserCreatedDomainEventHandler

---
+++ src/HomeInventory/HomeInventory.Application/Cqrs/DomainEvents/UserCreatedDomainEventHandler.cs
@@ -0,0 +1,16 @@
+﻿using HomeInventory.Domain.Aggregates;
+using MediatR;
+
+namespace HomeInventory.Application.Cqrs.DomainEvents;
+
+internal class UserCreatedDomainEventHandler : INotificationHandler<UserCreatedDomainEvent>
+{
+    public UserCreatedDomainEventHandler()
+    {
+    }
+
+    public Task Handle(UserCreatedDomainEvent notification, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+}
--- src/HomeInventory/HomeInventory.Infrastructure/Persistence/DatabaseContext.cs
+++ src/HomeInventory/HomeInventory.Infrastructure/Persistence/DatabaseContext.cs
@@ -1,15 +1,19 @@
 ﻿using HomeInventory.Domain.Primitives;
 using HomeInventory.Infrastructure.Persistence.Models;
 using HomeInventory.Infrastructure.Persistence.Models.Configurations;
+using MediatR;
 using Microsoft.EntityFrameworkCore;
 
 namespace HomeInventory.Infrastructure.Persistence;
 
 internal class DatabaseContext : DbContext, IDatabaseContext, IUnitOfWork
 {
-    public DatabaseContext(DbContextOptions<DatabaseContext> options)
+    private readonly IPublisher _publisher;
+
+    public DatabaseContext(DbContextOptions<DatabaseContext> options, IPublisher publisher)
         : base(options)
     {
+        _publisher = publisher;
     }
 
     public required DbSet<UserModel> Users { get; init; }
@@ -17,5 +21,21 @@ internal class DatabaseContext : DbContext, IDatabaseContext, IUnitOfWork
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         modelBuilder.ApplyConfiguration(new UserModelConfiguration());
+    }
+
+    public override async Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+    {
+        var domainEvents = ChangeTracker.Entries<IAggregateRoot>()
+            .Select(entry => entry.Entity)
+            .Where(root => root.DomainEvents.Count > 0)
+            .SelectMany(root => root.DomainEvents);
+
+        var savedCount = await base.SaveChangesAsync(cancellationToken);
+        foreach (var domainEvent in domainEvents)
+        {
+            await _publisher.Publish(domainEvent, cancellationToken);
+        }
+
+        return savedCount;
     }
 }
