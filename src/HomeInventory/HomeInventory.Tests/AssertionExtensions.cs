b1c60fdbe44f6a63d48d1056b760f165c9c6f7ae
Serhii A. Hrytsenko <gritcsenko@users.noreply.github.com>
2023-06-09 01:06:01 -0400

    Fix S4136: All 'Should' method overloads should be adjacent.

--- src/HomeInventory/HomeInventory.Tests/AssertionExtensions.cs
+++ src/HomeInventory/HomeInventory.Tests/AssertionExtensions.cs
@@ -11,15 +11,15 @@ internal static class AssertionExtensions
 
     public static JsonElementAssertions Should(this JsonElement actualValue) => new(actualValue);
 
-    public static AndWhichConstraint<ObjectAssertions, JsonElement> BeJsonElement(this ObjectAssertions assertions) => new(assertions, assertions.BeAssignableTo<JsonElement>().Subject);
-
     public static ServiceCollectionAssertions Should(this IServiceCollection actualValue) => new(actualValue);
 
     public static OkResultAssertions<TValue> Should<TValue>(this Ok<TValue> actualValue) => new(actualValue);
 
-    public static DictionaryAssertions ShouldBeDictionaryAnd(this IDictionary actualValue) => new(actualValue);
-
     public static OptionAssertions<T> Should<T>(this Optional<T> actualValue)
         where T : notnull =>
         new(actualValue);
+
+    public static AndWhichConstraint<ObjectAssertions, JsonElement> BeJsonElement(this ObjectAssertions assertions) => new(assertions, assertions.BeAssignableTo<JsonElement>().Subject);
+
+    public static DictionaryAssertions ShouldBeDictionaryAnd(this IDictionary actualValue) => new(actualValue);
 }
