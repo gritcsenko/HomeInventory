d48e82b974150333949ae3a054bb0210288b5927
Serhii A. Hrytsenko <gritcsenko@users.noreply.github.com>
2023-06-08 23:16:13 -0400

    Fix CA1815, S1206, and S4035

---
+++ src/HomeInventory/HomeInventory.Domain.Primitives/HashCodeExtensions.cs
@@ -0,0 +1,10 @@
+﻿namespace HomeInventory.Domain.Primitives;
+
+internal static class HashCodeExtensions
+{
+    public static HashCode Combine(this HashCode hash, object obj)
+    {
+        hash.Add(obj);
+        return hash;
+    }
+}
