6875abf12330c8b5958ce855135f2dd89be1ff39
Serhii A. Hrytsenko <gritcsenko@users.noreply.github.com>
2023-06-08 23:17:02 -0400

    Fix S4035: Seal class 'Equatable' or implement 'IEqualityComparer<T>' instead.

--- src/HomeInventory/HomeInventory.Domain.Primitives/Equatable.cs
+++ src/HomeInventory/HomeInventory.Domain.Primitives/Equatable.cs
@@ -1,5 +1,6 @@
 ﻿namespace HomeInventory.Domain.Primitives;
 
+[System.Diagnostics.CodeAnalysis.SuppressMessage("Major Code Smell", "S4035:Classes implementing \"IEquatable<T>\" should be sealed", Justification = "Designed to be a base class")]
 public abstract class Equatable<TSelf> : IEquatable<TSelf>
     where TSelf : Equatable<TSelf>
 {
