7e6fc100cbb978519894efa95586370ae94480ab
Serhii A. Hrytsenko <gritcsenko@users.noreply.github.com>
2023-06-08 23:46:38 -0400

    Suppress S3011

--- src/HomeInventory/HomeInventory.Domain.Primitives/ReflectionMethods.cs
+++ src/HomeInventory/HomeInventory.Domain.Primitives/ReflectionMethods.cs
@@ -2,8 +2,9 @@
 
 namespace HomeInventory.Domain.Primitives;
 
-public static class ReflectionMethods
+internal static class ReflectionMethods
 {
+    [System.Diagnostics.CodeAnalysis.SuppressMessage("Major Code Smell", "S3011:Reflection should not be used to increase accessibility of classes, methods, or fields", Justification = "Designed to call internal constructors")]
     private const BindingFlags _createInstanceBindingAttr = BindingFlags.Instance | BindingFlags.NonPublic | BindingFlags.Public | BindingFlags.CreateInstance;
 
     public static T? CreateInstance<T>(params object?[]? args)
