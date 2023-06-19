1be232f0a3917314683fe7936ad3f94042748fa0
Serhii A. Hrytsenko <gritcsenko@users.noreply.github.com>
2023-06-19 12:18:54 -0400

    Fix FluentOptionsValidator formatting

--- src/HomeInventory/HomeInventory.Web/FluentOptionsValidator.cs
+++ src/HomeInventory/HomeInventory.Web/FluentOptionsValidator.cs
@@ -5,40 +5,44 @@ using Microsoft.Extensions.Options;
 namespace HomeInventory.Web;
 
 internal sealed class FluentOptionsValidator<TOptions> : IValidateOptions<TOptions>
-        where TOptions : class
+    where TOptions : class
+{
+    private readonly Action<ValidationStrategy<TOptions>>? _validationOptions;
+
+    public FluentOptionsValidator(string? name, IValidator<TOptions> validator, Action<ValidationStrategy<TOptions>>? validationOptions)
     {
-        private readonly Action<ValidationStrategy<TOptions>>? _validationOptions;
-
-        public FluentOptionsValidator(string? name, IValidator<TOptions> validator, Action<ValidationStrategy<TOptions>>? validationOptions)
-        {
-            Name = name;
-            Validator = validator;
-            _validationOptions = validationOptions;
-        }
-
-        public string? Name { get; }
-
-        public IValidator<TOptions> Validator { get; }
-
-        public ValidateOptionsResult Validate(string? name, TOptions options)
-        {
-            // null name is used to configure all named options
-            if (Name != null && name != Name)
-            {
-                // ignored if not validating this instance
-                return ValidateOptionsResult.Skip;
-            }
-
-            var result = _validationOptions is null
-                ? Validator.Validate(options)
-                : Validator.Validate(options, _validationOptions);
-
-
-            if (result.IsValid)
-            {
-                return ValidateOptionsResult.Success;
-            }
-
-            return ValidateOptionsResult.Fail(result.ToString());
-        }
+        Name = name;
+        Validator = validator;
+        _validationOptions = validationOptions;
     }
+
+    public string? Name { get; }
+
+    public IValidator<TOptions> Validator { get; }
+
+    public ValidateOptionsResult Validate(string? name, TOptions options)
+    {
+        // null name is used to configure all named options
+        if (name == Name || name is null)
+        {
+            return ValidateCore(options);
+        }
+
+        // ignored if not validating this instance
+        return ValidateOptionsResult.Skip;
+    }
+
+    private ValidateOptionsResult ValidateCore(TOptions options)
+    {
+        var result = _validationOptions is null
+            ? Validator.Validate(options)
+            : Validator.Validate(options, _validationOptions);
+
+        if (result.IsValid)
+        {
+            return ValidateOptionsResult.Success;
+        }
+
+        return ValidateOptionsResult.Fail(result.ToString());
+    }
+}
