38521d4a267eb7c7b9d329ba2ac2eaa5c41ead94
Serhii A. Hrytsenko <gritcsenko@users.noreply.github.com>
2023-06-21 22:30:28 -0400

    Add AppBuilder

---
+++ src/HomeInventory/HomeInventory.Api/AppBuilder.cs
@@ -0,0 +1,31 @@
+﻿using HomeInventory.Application;
+using HomeInventory.Domain;
+using HomeInventory.Infrastructure;
+using HomeInventory.Web;
+
+namespace HomeInventory.Api;
+
+internal class AppBuilder
+{
+    private readonly WebApplicationBuilder _builder;
+
+    public AppBuilder(string[] args)
+    {
+        _builder = WebApplication.CreateBuilder(args);
+    }
+
+    public WebApplication Build()
+    {
+        _builder.Services
+          .AddSerilog(_builder.Configuration)
+          .AddMediatR()
+          .AddDomain()
+          .AddInfrastructure()
+          .AddApplication()
+          .AddWeb();
+
+        return _builder
+            .Build()
+            .UseWeb();
+    }
+}
--- src/HomeInventory/HomeInventory.Api/Program.cs
+++ src/HomeInventory/HomeInventory.Api/Program.cs
@@ -1,31 +1,10 @@
 using HomeInventory.Api;
-using HomeInventory.Application;
 using HomeInventory.Core;
-using HomeInventory.Domain;
-using HomeInventory.Infrastructure;
-using HomeInventory.Web;
-using Serilog;
 
 using var log = SerilogConfigurator.CreateBootstrapLogger();
 
 await Execute.AndCatchAsync(
-    async () =>
-    {
-        var builder = WebApplication.CreateBuilder(args);
-
-        builder.Services
-            .AddSerilog(builder.Configuration)
-            .AddMediatR()
-            .AddDomain()
-            .AddInfrastructure()
-            .AddApplication()
-            .AddWeb();
-
-        await builder
-            .Build()
-            .UseWeb()
-            .RunAsync();
-    },
+    async () => await new AppBuilder(args).Build().RunAsync(),
     (Exception ex) => log.Fatal(ex, "An unhandled exception occurred during bootstrapping"));
 
 public partial class Program
