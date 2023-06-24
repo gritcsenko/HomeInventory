7abbc89edace4a81df0d712362a873a90a00f062
Serhii A. Hrytsenko <gritcsenko@users.noreply.github.com>
2023-06-24 17:12:47 -0400

    Cleanup EnumerableExtensions

--- src/HomeInventory/HomeInventory.Core/EnumerableExtensions.cs
+++ src/HomeInventory/HomeInventory.Core/EnumerableExtensions.cs
@@ -1,6 +1,4 @@
-﻿using System.Runtime.CompilerServices;
-
-namespace HomeInventory.Core;
+﻿namespace HomeInventory.Core;
 
 public static class EnumerableExtensions
 {
@@ -11,64 +9,4 @@ public static class EnumerableExtensions
     public static IEnumerable<T> EmptyIfNull<T>(this IEnumerable<T>? source) => source ?? Array.Empty<T>();
 
     public static IReadOnlyCollection<T> ToReadOnly<T>(this IEnumerable<T> source) => source as IReadOnlyCollection<T> ?? source.ToArray();
-
-    public static Queue<T> ToQueue<T>(this IEnumerable<T> source) => new(source);
-
-    public static async IAsyncEnumerable<TResult> ParallelSelectAsync<TSource, TResult>(
-        this IEnumerable<TSource> items,
-        Func<TSource, CancellationToken, Task<TResult>> selectAsync,
-        [EnumeratorCancellation] CancellationToken cancellationToken = default)
-    {
-        using var cancellationTokenSource = CancellationTokenSource.CreateLinkedTokenSource(cancellationToken);
-
-        var remainingTasks = items.Select(x => selectAsync(x, cancellationTokenSource.Token)).ToHashSet();
-
-        try
-        {
-            while (remainingTasks.Count > 0)
-            {
-                var completedTask = await Task.WhenAny(remainingTasks);
-                if (remainingTasks.Remove(completedTask))
-                    yield return completedTask.GetAwaiter().GetResult();
-            }
-        }
-        finally
-        {
-            cancellationTokenSource.Cancel();
-            if (remainingTasks.Count > 0)
-            {
-                await Execute.AndCatchAsync(
-                    async () => await Task.WhenAll(remainingTasks),
-                    (OperationCanceledException ex) => { });
-            }
-        }
-    }
-
-    public static async IAsyncEnumerable<TResult> SelectAsync<TSource, TResult>(
-        this IEnumerable<TSource> items,
-        Func<TSource, CancellationToken, Task<TResult>> selectAsync,
-        [EnumeratorCancellation] CancellationToken cancellationToken = default)
-    {
-        using var cancellationTokenSource = CancellationTokenSource.CreateLinkedTokenSource(cancellationToken);
-
-        var remainingTasks = items.Select(x => selectAsync(x, cancellationTokenSource.Token)).ToQueue();
-
-        try
-        {
-            while (remainingTasks.Count > 0)
-            {
-                yield return await remainingTasks.Dequeue();
-            }
-        }
-        finally
-        {
-            cancellationTokenSource.Cancel();
-            if (remainingTasks.Count > 0)
-            {
-                await Execute.AndCatchAsync(
-                    async () => await Task.WhenAll(remainingTasks),
-                    (OperationCanceledException ex) => { });
-            }
-        }
-    }
 }
