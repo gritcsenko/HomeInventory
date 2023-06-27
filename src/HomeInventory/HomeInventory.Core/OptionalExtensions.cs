9b4c1faefa62c2253f4025dc1b5336eecc0bef14
Serhii A. Hrytsenko <gritcsenko@users.noreply.github.com>
2023-06-27 08:44:56 -0400

    Add OptionalExtensions

---
+++ src/HomeInventory/HomeInventory.Core/OptionalExtensions.cs
@@ -0,0 +1,42 @@
+﻿using System.Runtime.Versioning;
+
+namespace HomeInventory.Core;
+
+public static class OptionalExtensions
+{
+    [RequiresPreviewFeatures]
+    public static async Task<Optional<T>> Tap<T>(this Task<Optional<T>> optionalTask, Action<T> action)
+    {
+        var optional = await optionalTask;
+        return optional.Tap(action);
+    }
+
+    [RequiresPreviewFeatures]
+    public static async Task<Optional<T>> Tap<T>(this Task<Optional<T>> optionalTask, Func<T, Task> asyncAction)
+    {
+        var optional = await optionalTask;
+        return await optional.Tap(asyncAction);
+    }
+
+    [RequiresPreviewFeatures]
+    public static Optional<T> Tap<T>(this Optional<T> optional, Action<T> action)
+    {
+        if (optional.TryGet(out var value) && value is not null)
+        {
+            action(value);
+        }
+
+        return optional;
+    }
+
+    [RequiresPreviewFeatures]
+    public static async Task<Optional<T>> Tap<T>(this Optional<T> optional, Func<T, Task> asyncAction)
+    {
+        if (optional.TryGet(out var value) && value is not null)
+        {
+            await asyncAction(value);
+        }
+
+        return optional;
+    }
+}
