6cf0d9dc48e954786b75ea88466309c73061f153
Serhii A. Hrytsenko <gritcsenko@users.noreply.github.com>
2023-07-11 08:10:41 -0400

    Add missing ConfigurationAppliers

---
+++ src/HomeInventory/HomeInventory.Infrastructure/Persistence/Models/Configurations/ProductModelConfigurationApplier.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace HomeInventory.Infrastructure.Persistence.Models.Configurations;
+
+internal class ProductModelConfigurationApplier : IDatabaseConfigurationApplier
+{
+    public void ApplyConfigurationTo(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfiguration(new ProductModelConfiguration());
+    }
+}
