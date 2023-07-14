74422f4fbc92454f78e3b1eb85d9b782e12a06a1
Serhii A. Hrytsenko <gritcsenko@users.noreply.github.com>
2023-07-13 20:45:26 -0400

    Fix traits for PoolTests

--- src/HomeInventory/HomeInventory.Tests/Core/PoolTests.cs
+++ src/HomeInventory/HomeInventory.Tests/Core/PoolTests.cs
@@ -2,7 +2,7 @@
 
 namespace HomeInventory.Tests.Domain.Primitives;
 
-[Trait("Category", "Unit")]
+[UnitTest]
 public class PoolTests : BaseTest
 {
     private readonly IPoolObjectActivator<object> _activator = Substitute.For<IPoolObjectActivator<object>>();
