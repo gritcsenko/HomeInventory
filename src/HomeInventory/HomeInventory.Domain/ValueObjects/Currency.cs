707a55f29390a0a6307df45cfd58351912b6b98e
Serhii A. Hrytsenko <gritcsenko@users.noreply.github.com>
2023-07-27 13:37:19 -0400

    Add Money and currency

---
+++ src/HomeInventory/HomeInventory.Domain/ValueObjects/Currency.cs
@@ -0,0 +1,14 @@
+﻿using HomeInventory.Domain.Primitives;
+
+namespace HomeInventory.Domain.ValueObjects;
+
+public sealed class Currency : ValueObject<Currency>
+{
+    public Currency(string name)
+        : base(name)
+    {
+        Name = name;
+    }
+
+    public string Name { get; }
+}
---
+++ src/HomeInventory/HomeInventory.Domain/ValueObjects/Money.cs
@@ -0,0 +1,17 @@
+﻿using HomeInventory.Domain.Primitives;
+
+namespace HomeInventory.Domain.ValueObjects;
+
+public sealed class Money : ValueObject<Money>
+{
+    public Money(decimal amount, Currency currency)
+        : base(amount, currency)
+    {
+        Amount = amount;
+        Currency = currency;
+    }
+
+    public decimal Amount { get; }
+
+    public Currency Currency { get; }
+}
