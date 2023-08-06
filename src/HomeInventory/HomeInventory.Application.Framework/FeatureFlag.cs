42bac8901dd0fde784d291211a2e6dc093e5ad8f
Serhii A. Hrytsenko <gritcsenko@users.noreply.github.com>
2023-08-05 20:43:08 -0400

    Add feature flags

---
+++ src/HomeInventory/HomeInventory.Application.Framework/FeatureFlag.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.FeatureManagement;
+
+namespace HomeInventory.Application.Framework;
+
+public sealed class FeatureFlag : IFeatureFlag
+{
+    internal FeatureFlag(string name)
+    {
+        Name = name;
+    }
+
+    public string Name { get; }
+
+    public async Task<bool> IsEnabledAsync(IFeatureManager manager, CancellationToken cancellationToken = default)
+    {
+        return await manager.IsEnabledAsync(Name, cancellationToken);
+    }
+
+    public static IFeatureFlag Create(string name) => new FeatureFlag(name);
+
+    public static IFeatureFlag Create<TContext>(string name, TContext context) => new FeatureFlag<TContext>(name, context);
+}
+
+public sealed class FeatureFlag<TContext> : IFeatureFlag
+{
+    internal FeatureFlag(string name, TContext context)
+    {
+        Name = name;
+        Context = context;
+    }
+
+    public string Name { get; }
+
+    public TContext Context { get; }
+
+    public async Task<bool> IsEnabledAsync(IFeatureManager manager, CancellationToken cancellationToken = default)
+    {
+        return await manager.IsEnabledAsync(Name, Context, cancellationToken);
+    }
+}
---
+++ src/HomeInventory/HomeInventory.Application.Framework/IFeatureFlag.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.FeatureManagement;
+
+namespace HomeInventory.Application.Framework;
+
+public interface IFeatureFlag
+{
+    string Name { get; }
+
+    Task<bool> IsEnabledAsync(IFeatureManager manager, CancellationToken cancellationToken = default);
+}
