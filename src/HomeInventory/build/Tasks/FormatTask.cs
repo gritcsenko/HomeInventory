e360d0500061ccbaf962bf6ce3280c51929327b1
Serhii A. Hrytsenko <gritcsenko@users.noreply.github.com>
2023-08-25 09:15:26 -0400

    Use Cake for Format

---
+++ src/HomeInventory/build/Tasks/FormatTask.cs
@@ -0,0 +1,21 @@
+﻿using Cake.Common.Tools.DotNet;
+using Cake.Common.Tools.DotNet.Format;
+using Cake.Frosting;
+
+namespace Build.Tasks;
+
+[TaskName("Format")]
+public sealed class FormatTask : FrostingTask<BuildContext>
+{
+    public override void Run(BuildContext context)
+    {
+        context.DotNetFormat(
+            context.Solution,
+            new DotNetFormatSettings
+            {
+                VerifyNoChanges = true,
+                Severity = DotNetFormatSeverity.Error,
+                Verbosity = DotNetVerbosity.Diagnostic,
+            });
+    }
+}
