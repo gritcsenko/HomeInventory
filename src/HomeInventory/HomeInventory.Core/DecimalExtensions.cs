cf3dfe843aef2bee5655aec672cc9b1036a6a12e
Serhii A. Hrytsenko <gritcsenko@gmail.com>
2023-11-17 02:25:01 -0500

    Add IsPow10

---
+++ src/HomeInventory/HomeInventory.Core/DecimalExtensions.cs
@@ -0,0 +1,36 @@
+﻿namespace HomeInventory.Core;
+
+public static class DecimalExtensions
+{
+    public static bool IsPow10(this decimal d)
+    {
+        Span<int> bits = stackalloc int[4];
+        _ = decimal.GetBits(d, bits);
+        var value = (Int128)bits[0] + ((Int128)bits[1] << 32) + ((Int128)bits[2] << 64);
+        if (value == 1)
+        {
+            return true;
+        }
+
+        if (value < 10)
+        {
+            return false;
+        }
+
+        while (value >= 10)
+        {
+            var (div, rem) = Int128.DivRem(value, 10);
+            if (rem != 0)
+            {
+                return false;
+            }
+            if (div == 1)
+            {
+                return true;
+            }
+            value = div;
+        }
+
+        return false;
+    }
+}
