1a07522c34aef6771612303b65e092135f50f189
Serhii A. Hrytsenko <gritcsenko@gmail.com>
2024-01-16 10:06:28 -0500

    Update Logging

--- src/HomeInventory/HomeInventory.Web/LogEvents.cs
+++ src/HomeInventory/HomeInventory.Web/LogEvents.cs
@@ -2,19 +2,11 @@
 
 namespace HomeInventory.Web;
 
-internal static class LogEvents
+internal static partial class LogEvents
 {
-    private static readonly EventId _newCorrelationId = new(4, "CorrelationId generated");
+    [LoggerMessage(Level = LogLevel.Information, Message = "New {CorrelationId} was generated")]
+    public static partial void CorrelationIdGenerated(this ILogger logger, string correlationId);
 
-    private static readonly EventId _correlationIdReturned = new(4, "CorrelationId returned");
-
-    private static readonly Action<ILogger, string, Exception?> _correlationIdGenerated = LoggerMessage.Define<string>(LogLevel.Information, _newCorrelationId, "New {CorrelationId} was generated");
-
-    private static readonly Action<ILogger, string, Exception?> _correlationIdReturnedToCaller = LoggerMessage.Define<string>(LogLevel.Information, _correlationIdReturned, "{CorrelationId} was returned to the caller");
-
-    public static void CorrelationIdGenerated(this ILogger logger, object value) =>
-        _correlationIdGenerated(logger, value.ToString() ?? string.Empty, null);
-
-    public static void CorrelationIdReturned(this ILogger logger, object value) =>
-        _correlationIdReturnedToCaller(logger, value.ToString() ?? string.Empty, null);
+    [LoggerMessage(Level = LogLevel.Information, Message = "{CorrelationId} was returned to the caller")]
+    public static partial void CorrelationIdReturned(this ILogger logger, string correlationId);
 }
