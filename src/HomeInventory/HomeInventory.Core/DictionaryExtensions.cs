9271e0d7fe7a9fc8e5dc6fc211df7f46e5011cb9
Serhii A. Hrytsenko <gritcsenko@gmail.com>
2024-02-04 15:08:03 -0500

    Remove useless null checks

--- src/HomeInventory/HomeInventory.Core/DictionaryExtensions.cs
+++ src/HomeInventory/HomeInventory.Core/DictionaryExtensions.cs
@@ -4,9 +4,9 @@ public static class DictionaryExtensions
 {
     public static Optional<TValue> GetValueOptional<TKey, TValue>(this IReadOnlyDictionary<TKey, TValue> dictionary, TKey key)
         where TKey : notnull =>
-        dictionary?.TryGetValue(key, out var value) == true ? value : Optional.None<TValue>();
+        dictionary.TryGetValue(key, out var value) ? value : Optional.None<TValue>();
 
     public static Optional<TValue> GetValueOptional<TKey, TValue>(this IDictionary<TKey, TValue> dictionary, TKey key)
         where TKey : notnull =>
-        dictionary?.TryGetValue(key, out var value) == true ? value : Optional.None<TValue>();
+        dictionary.TryGetValue(key, out var value) ? value : Optional.None<TValue>();
 }
