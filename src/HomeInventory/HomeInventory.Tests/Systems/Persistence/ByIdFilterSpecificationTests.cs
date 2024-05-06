b04622a045b03e02d4be592ea5ae6d1738ca0ce5
Serhii A. Hrytsenko <gritcsenko@users.noreply.github.com>
2024-05-06 15:37:44 -0400

    Fix build

--- src/HomeInventory/HomeInventory.Tests/Systems/Persistence/ByIdFilterSpecificationTests.cs
+++ src/HomeInventory/HomeInventory.Tests/Systems/Persistence/ByIdFilterSpecificationTests.cs
@@ -49,7 +49,7 @@ public class ByIdFilterSpecificationTests : BaseDatabaseContextTest
         var user = Fixture.Build<UserModel>()
             .With(m => m.Id, _id)
             .Create();
-        Context.Set<UserModel>().Add(user);
+        await Context.Set<UserModel>().AddAsync(user, Cancellation.Token);
         await Context.SaveChangesAsync();
         var sut = CreateSut();
 
@@ -62,7 +62,7 @@ public class ByIdFilterSpecificationTests : BaseDatabaseContextTest
     public async Task ExecuteAsync_Should_NotSatisfyWithWrongId()
     {
         var user = Fixture.Create<UserModel>();
-        Context.Set<UserModel>().Add(user);
+        await Context.Set<UserModel>().AddAsync(user, Cancellation.Token);
         await Context.SaveChangesAsync();
         var sut = CreateSut();
 
