66ebad9e5630c653e7b2de358b7e05343e39cbe0
Serhii A. Hrytsenko <gritcsenko@users.noreply.github.com>
2025-04-21 14:47:55 -0400

    Revert namespace change
    Signed-off-by: Serhii A. Hrytsenko <gritcsenko@users.noreply.github.com>

--- src/HomeInventory/HomeInventory.Application.Framework/ApplicationFrameworkServiceCollectionExtensions.cs
+++ src/HomeInventory/HomeInventory.Application.Framework/ApplicationFrameworkServiceCollectionExtensions.cs
@@ -1,14 +1,17 @@
 ﻿using System.Reflection;
-using Microsoft.Extensions.DependencyInjection;
+using HomeInventory.Application.Framework;
+using HomeInventory.Application.Framework.Mapping;
 
-namespace HomeInventory.Application.Framework;
+namespace Microsoft.Extensions.DependencyInjection;
 
 public static class ApplicationFrameworkServiceCollectionExtensions
 {
-    public static IServiceCollection AddMappingAssemblySource(this IServiceCollection services, params Assembly[] assemblies)
-    {
-        ////services.TryAddSingleton(typeof(TypeConverterAdapter<,,>));
-        services.AddSingleton<IMappingAssemblySource>(_ => new MappingAssemblySource(assemblies));
-        return services;
-    }
+    public static IServiceCollection AddMappingAssemblySource(this IServiceCollection services, params Assembly[] assemblies) =>
+        services
+            .AddMappingTypeConverter()
+            .AddSingleton<IMappingAssemblySource>(_ => new MappingAssemblySource(assemblies));
+
+    private static IServiceCollection AddMappingTypeConverter(this IServiceCollection services) =>
+        services
+            .AddSingleton(typeof(TypeConverterAdapter<,,>));
 }
